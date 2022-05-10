4f649485276970a446929c893c319f3bb969dd90
davidsteinbruch <davidsteinb@gmail.com>
2022-05-10 09:29:10 -0400

    Creates a CRUD Web API for books using .NET Core

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace Marketplace.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string Image { get; set; }
+
+        public decimal Price { get; set; }
+        
+        public Book(long id, string title, string description, string author, string image, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Author = author;
+            Image = image;
+            Price = price;
+        }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+    : base(options)
+        {
+            if(!Books.Any())
+            {
+                Books.AddRange(new Book(1, "Rich Dad Poor Dad", "What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!", "Robert T. Kiyosaki", "https://images-na.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SX330_BO1,204,203,200_.jpg", 9),
+                    new Book(2, "Learn Microsoft Azure", "Build, manage, and scale cloud applications using the Azure ecosystem", "Mohamed Wali", "https://images-na.ssl-images-amazon.com/images/I/51hMZaeInaL._SX404_BO1,204,203,200_.jpg", 57));
+                this.SaveChanges();
+            }            
+        }
+
+        public DbSet<Book> Books { get; set; } = null!;
+    }
+}
